c5671e6c5c96bdbd98ed398b8770f5e5ca4ca5b3
Tanisha0528 <tanishabansal0528@gmail.com>
2022-11-24 16:11:37 +0530

    Product Project

--- src/test/java/com/SpringBootReactive/MongoDB/React/Products/ProductsApplicationTests.java
+++ src/test/java/com/SpringBootReactive/MongoDB/React/Products/ProductsApplicationTests.java
@@ -5,7 +5,7 @@ import com.SpringBootReactive.MongoDB.React.Products.dto.ProductDto;
 import com.SpringBootReactive.MongoDB.React.Products.entity.Product;
 import com.SpringBootReactive.MongoDB.React.Products.service.ProductService;
 import com.SpringBootReactive.MongoDB.React.Products.util.AppUtils;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
@@ -21,7 +21,9 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -40,29 +42,6 @@ public class ProductsApplicationTests {
 
 	@MockBean
 	private ProductService service;
-/*
-
-	@Test
-	public void saveProductTest()
-	{
-		List<Integer> pincode = new ArrayList<Integer>();
-
-		// Initialize an ArrayList with add()
-		pincode.add(1234);
-		pincode.add(12345);
-		pincode.add(123456);
-
-		ProductDto productDtoMono=new ProductDto("1","iphone","Mobile",1,100000,pincode);
-
-		when(service.saveProduct(productDtoMono).thenReturn(productDtoMono));
-
-		client.post().uri("/products/saveProduct")
-				.body(Mono.just(productDtoMono), ProductDto.class)
-				.exchange()
-				.expectStatus()
-				.isOk();
-	}
-*/
 
 	@Test
 	public void getAllProductsTest()
@@ -78,7 +57,7 @@ public class ProductsApplicationTests {
 
 		Flux<ProductDto> productDtoFlux=Flux.just(
 				new ProductDto("1","iphone","Mobile",1,100000,pincode),
-				new ProductDto("1","samsung","Mobile",1,10000,pincode)
+				new ProductDto("2","samsung","Mobile",1,10000,pincode)
 
 
 		);
@@ -98,7 +77,7 @@ public class ProductsApplicationTests {
 		StepVerifier.create(responseBody)
 				.expectSubscription()
 				.expectNext(new ProductDto("1","iphone","Mobile",1,100000,pincode))
-				.expectNext(new ProductDto("1","samsung","Mobile",1,10000,pincode))
+				.expectNext(new ProductDto("2","samsung","Mobile",1,10000,pincode))
 				.verifyComplete();
 	}
 
@@ -134,9 +113,9 @@ public class ProductsApplicationTests {
 				.verifyComplete();
 	}
 
-/*
+
 	@Test
-	public void updateProductByIdTest()
+	public void getProductByNameTest()
 	{
 		List<Integer> pincode = new ArrayList<Integer>();
 
@@ -145,28 +124,142 @@ public class ProductsApplicationTests {
 		pincode.add(12345);
 		pincode.add(123456);
 
-		Mono<ProductDto> productDtoMono=Mono.just(new ProductDto("1","samsung","Mobile",1,10000,pincode));
+		Flux<ProductDto> productDtoFlux=Flux.just(new ProductDto("1","samsung","Mobile",1,10000,pincode));
 
-		when(service.updateProductById(productDtoMono,"1")).thenReturn(ResponseEntity.status(HttpStatus.OK).body(productDtoMono));
+				//for testing purpose we are saying when i pass any() id then the mentioned productDtoMono
+		//should be returned
+		when(service.getProductByName("samsung")).thenReturn(productDtoFlux);
 
-		client.put().uri("/products/updateProduct/1")
-				.body(Mono.just(productDtoMono), ProductDto.class)
+		//so we can pass any id
+		Flux<ProductDto> responseBody=client.get().uri("/products/getProductByName/samsung")
 				.exchange()
 				.expectStatus()
-				.isOk();
-	}*/
+				.isOk()
+				.returnResult(ProductDto.class)
+				.getResponseBody();
+
+		//just verifying one field
+		StepVerifier.create(responseBody)
+				.expectSubscription()
+				.expectNextMatches(p->p.getCategory().equals("Mobile"))
+				.verifyComplete();
+	}
+
+	@Test
+	public void getProductByCatgeoryTest()
+	{
+		List<Integer> pincode = new ArrayList<Integer>();
+
+		// Initialize an ArrayList with add()
+		pincode.add(1234);
+		pincode.add(12345);
+		pincode.add(123456);
+
+		Flux<ProductDto> productDtoFlux=Flux.just(new ProductDto("1","samsung","Mobile",1,10000,pincode),
+
+				new ProductDto("1","iphone","Mobile",1,80000,pincode));
+
+				//for testing purpose we are saying when i pass any() id then the mentioned productDtoMono
+		//should be returned
+		when(service.getProductByCategory("Mobile")).thenReturn(productDtoFlux);
+
+		//so we can pass any id
+		Flux<ProductDto> responseBody=client.get().uri("/products/getProductByCategory/Mobile")
+				.exchange()
+				.expectStatus()
+				.isOk()
+				.returnResult(ProductDto.class)
+				.getResponseBody();
+
+		//just verifying one field
+		StepVerifier.create(responseBody)
+				.expectSubscription()
+				.expectNextMatches(p->p.getName().equals("samsung"))
+				.expectNextMatches(p->p.getName().equals("iphone"))
+				.verifyComplete();
+	}
+	/*
+	@Test
+	public void getProductByPriceRangeTest()
+	{
+		List<Integer> pincode = new ArrayList<Integer>();
+
+		// Initialize an ArrayList with add()
+		pincode.add(1234);
+		pincode.add(12345);
+		pincode.add(123456);
+
+		Flux<ProductDto> productDtoFlux=Flux.just(new ProductDto("1","samsung","Mobile",1,10000,pincode),
+				new ProductDto("1","samsung","tv",1,100000,pincode),
+				new ProductDto("1","iphone","tv",1,80000,pincode));
+
+				//for testing purpose we are saying when i pass any() id then the mentioned productDtoMono
+		//should be returned
+		when(service.getProductsWithinPriceRange(10000.0,90000.0)).thenReturn(productDtoFlux);
+		Map<Object,String> map=new HashMap<>();
+		map.put(new Object("min"="10000","max"="90000"));
+		//so we can pass any id
+		Flux<ProductDto> responseBody=client.get().uri("/products/getProductsWithinPriceRange/")
+				.exchange()
+				.expectStatus()
+				.isOk()
+				.returnResult(ProductDto.class)
+				.getResponseBody();
+
+		//just verifying one field
+		StepVerifier.create(responseBody)
+				.expectSubscription()
+				.expectNextMatches(p->p.getName().equals("samsung"))
+				.expectNextMatches(p->p.getName().equals("iphone"))
+				.verifyComplete();
+	}
+*/
+
 	@Test
 	public void deleteProductTest() {
-		//given(service.deleteProductById(any())).willReturn(Mono.empty());
-
-		//Mono<Void> voidReturn  = Mono.empty();
-		when(service.deleteProductById(any()))
-				.thenReturn(Mono.just(new ResponseEntity<>(HttpStatus.OK)));
+		given(service.deleteProductById(any())).willReturn(Mono.empty());
 		client.delete().uri("/products/deleteProduct/1")
 				.exchange()
 				.expectStatus()
 				.isOk();
 
 	}
+	@Test
+	public void addProductsTest()
+	{
+		List<Integer> pincode = new ArrayList<Integer>();
 
+		// Initialize an ArrayList with add()
+		pincode.add(1234);
+		pincode.add(12345);
+		pincode.add(123456);
+
+
+		ProductDto productDto=new ProductDto("1","samsung","Mobile",1,10000,pincode);
+		when(service.saveProduct(productDto)).thenReturn(Mono.just(new ResponseEntity<>(HttpStatus.OK)));
+
+		client.post().uri("/products/saveProduct")
+				.body(Mono.just(productDto), ProductDto.class)
+				.exchange()
+				.expectStatus()
+				.isOk();
+	}
+	@Test
+	public void updateProductTest()
+	{List<Integer> pincode = new ArrayList<Integer>();
+
+		// Initialize an ArrayList with add()
+		pincode.add(1234);
+		pincode.add(12345);
+		pincode.add(123456);
+		Mono<ProductDto> productDtoMono=Mono.just(new ProductDto("1","samsung","Mobile",1,10000,pincode));
+
+		when(service.updateProductById(productDtoMono,"1")).thenReturn(Mono.just(new ResponseEntity<>(HttpStatus.OK)));
+
+		client.put().uri("/products/updateProduct/1")
+				.body(Mono.just(productDtoMono), ProductDto.class)
+				.exchange()
+				.expectStatus()
+				.isOk();
+	}
 }
